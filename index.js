84fd367eaf081d80fa4c302b49b6f87719eeee43
Longng000 <long.nguyen9@Griffithuni.edu.au>
2024-08-01 12:04:08 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,3 @@ function convUpperCaseDeclarative(value) {
     return value.toUpperCase();
 }
 console.log(convUpperCaseDeclarative('hello'));  
-
-const convUpperCaseExpression = function(value) {
-    return value.toUpperCase();
-};
-console.log(convUpperCaseExpression('hello'));  
-
-const convUpperCaseArrow = value => value.toUpperCase();
-console.log(convUpperCaseArrow('hello')); 
